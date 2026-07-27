9a7050cd8b72ec5aad1e1be497d320222a53a051
Saidat2007 <belloobilikisu@gmail.com>
2026-07-27 20:19:52 +0100

    Fix cloudinary issues

--- server/routes/cloudinary.js
+++ server/routes/cloudinary.js
@@ -1,7 +1,8 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary').v2;
 
+// Configure Cloudinary credentials
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -11,11 +12,10 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'hydroflood-reports', // This creates the folder in Cloudinary
-    allowed_formats: ['jpg', 'jpeg', 'png']
+    folder: 'hydroflood-reports', // This creates the folder automatically!
+    allowed_formats: ['jpg', 'png', 'jpeg']
   }
 });
 
 const upload = multer({ storage: storage });
-
 module.exports = upload;
